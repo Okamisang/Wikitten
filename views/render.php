c2b46eb69d8341c9df01057160e24cf4110880a9
Rhilip <rhilipruan@gmail.com>
2019-09-06 10:36:08 +0800

    移除PasteBin插件，禁止前端编辑

--- views/render.php
+++ views/render.php
@@ -1,13 +1,4 @@
 <div class="breadcrumbs">    
-    <div class="pull-right">
-        <?php if ($html && isset($source)): ?>
-            <a href="javascript:;" class="btn-black" id="toggle">Toggle source</a>
-        <?php endif ?>
-        <?php if ($use_pastebin): ?>
-            <a href="javascript:;" class="btn-black" id="create-pastebin" title="Create public Paste on PasteBin">Create public Paste</a>
-        <?php endif; ?>
-    </div>    
-
     <?php $path = array(); ?>
     <ul class="breadcrumb">
         <li>
@@ -33,9 +24,6 @@
 </div>
 
 <?php if ($html): ?>
-    <?php if ($use_pastebin): ?>
-    <div id="pastebin-notification" class="alert" style="display:none;"></div>
-    <?php endif; ?>
     <div id="render">
         <?php echo $html; ?>
     </div>
@@ -51,35 +39,7 @@
 <?php endif ?>
 
 <?php if (isset($source)): ?>
-    <?php if ($use_pastebin): ?>
-    <div id="pastebin-notification" class="alert" style="display:none;"></div>
-    <?php endif; ?>
-    <div id="source">
-        <?php if (ENABLE_EDITING): ?>
-            <div class="alert alert-info">
-                <i class="glyphicon glyphicon-pencil"></i> <strong>Editing is enabled</strong>. Use the "Save changes" button below the editor to commit modifications to this file.
-            </div>
-        <?php endif ?>
-
-        <form method="POST" action="<?php echo BASE_URL . "/?a=edit" ?>">
-            <input type="hidden" name="ref" value="<?php echo base64_encode($page['file']) ?>">
-            <textarea id="editor" name="source" class="form-control" rows="<?php echo substr_count($source, "\n") + 1; ?>"><?php echo $source; ?></textarea>
-
-            <?php if (ENABLE_EDITING): ?>
-                <div class="form-actions">
-                    <input type="submit" class="btn btn-warning btn-sm" id="submit-edits" value="Save Changes">
-                </div>
-            <?php endif ?>
-        </form>
-    </div>
-
     <script>
-        <?php if ($html): ?>
-            CodeMirror.defineInitHook(function () {
-                $('#source').hide();
-            });
-        <?php endif ?>
-
         var mode = false;
         var modes = {
             'md': 'markdown',
@@ -109,15 +69,9 @@
         var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
             lineNumbers: true,
             lineWrapping: true,
-            <?php if (USE_DARK_THEME): ?>
-            theme: 'tomorrow-night-bright',
-            <?php else: ?>
-            theme: 'default',
-            <?php endif; ?>
-            mode: mode
-            <?php if(!ENABLE_EDITING): ?>
-            ,readOnly: true
-            <?php endif ?>
+            theme: <?= USE_DARK_THEME ? 'tomorrow-night-bright' : 'default' ?>,
+            mode: mode,
+            readOnly: true
         });
 
         $('#toggle').click(function (event) {
@@ -129,31 +83,5 @@
             }
 
         });
-
-        <?php if ($use_pastebin): ?>
-        $('#create-pastebin').on('click', function (event) {
-            event.preventDefault();
-
-            $(this).addClass('disabled');
-
-            var notification = $('#pastebin-notification');
-            notification.removeClass('alert-info alert-error').html('').hide();
-
-            $.ajax({
-                type: 'POST',
-                url: '<?php echo BASE_URL . '/?a=createPasteBin'; ?>',
-                data: { ref: '<?php echo base64_encode($page['file']); ?>' },
-                context: $(this)
-            }).done(function(response) {                
-                $(this).removeClass('disabled');
-
-                if (response.status === 'ok') {
-                    notification.addClass('alert-info').html('Paste URL: ' + response.url).show();
-                } else {
-                    notification.addClass('alert-error').html('Error: ' + response.error).show();
-                }
-            });
-        });
-        <?php endif; ?>
     </script>
 <?php endif ?>
